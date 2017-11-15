b729fecc6fcd6ce6f28aea238bdc91b584cb6dc6
Vadim Bondaruk <wadbondaruk@gmail.com>
2017-11-15 12:16:43 +0300

    Add project files.

---
+++ StudentsApp/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using StudentsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace StudentsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //XDocument doc = new XDocument(@"C:\Users\msi\Downloads\C#_\Students.xml");
+
+            //foreach (var item in doc.Elements("Student"))
+            //{
+
+            //}
+            List<Student> elements = new List<Student>();
+            XmlRootAttribute xRoot = new XmlRootAttribute();
+            xRoot.ElementName = "Students";
+            xRoot.IsNullable = true;
+
+            var serializer = new XmlSerializer(typeof(List<Student>), xRoot);
+
+            using (var reader = new StreamReader(@"D:\PROJECTS\StudentsApp\StudentsApp\Data\Students.xml"))
+            {
+                 elements = (List<Student>)serializer.Deserialize(reader);
+            }
+
+            elements.Add(new Student { Id = 10, FirstName = "Valera", Gender = 0, Last = "XUI" });
+
+            using (var writer = new StreamWriter(@"D:\PROJECTS\StudentsApp\StudentsApp\Data\Students.xml"))
+            {
+                serializer.Serialize(writer, elements);
+            }
+        }
+    }
+}
---
+++ StudentsApp/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace StudentsApp.Models
+{
+    [Serializable]
+    public class Student
+    {
+        [XmlAttribute]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Last { get; set; }
+        public int Gender { get; set; }
+    }
+}
